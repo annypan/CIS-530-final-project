e7e0e8b9cef17a13e4073c63cbc84895741558a4
AnnyP <annipan@seas.upenn.edu>
2021-12-02 13:36:10 -0500

    Add cli for outputting metric for each ingredient

--- evaluate.py
+++ evaluate.py
@@ -52,6 +52,26 @@ def get_wrong_ingredients(pred_path, ground_truth_path):
     print("Wrong substitutes can now be found at evaluations/wrong_ingredients.json")
 
 
+def get_precision_recall_each_ingredient(pred_path, ground_truth_path):
+    with open(f'data/{ground_truth_path}') as f:
+        ground_truth = json.load(f)
+        with open(f'predictions/{pred_path}') as f_pred:
+            pred = json.load(f_pred)
+            results = {}
+            for ingredient in ground_truth:
+                precision = get_num_correct(
+                    ground_truth[ingredient], pred[ingredient]) / len(pred[ingredient])
+                recall = get_num_recalled(
+                    ground_truth[ingredient], pred[ingredient]) / len(ground_truth[ingredient])
+                results[ingredient] = {
+                    "precision": precision,
+                    "recall": recall
+                }
+    with open(f'evaluations/metrics_by_ingredient.json', 'w') as f_out:
+        json.dump(results, f_out, ensure_ascii=False)
+    print("Precision and recall for each ingredient can now be found at evaluations/metrics_by_ingredient.json")
+
+
 def get_num_correct(true, pred):
     num_correct = 0
     for ingredient in pred:
@@ -77,6 +97,9 @@ parser.add_argument("-gm", "--get_missing", dest="get_missing_ingredients",
                     required=False, help="set to True to output the missing ingredients")
 parser.add_argument("-gw", "--get_wrong", dest="get_wrong_ingredients",
                     required=False, help="set to True to output the wrong ingredients")
+parser.add_argument("-each", "--get_metric_for_each",
+                    dest="get_precision_recall_each_ingredient",
+                    required=False, help="set to True to output the precision and recall for each ingredient")
 
 args = parser.parse_args()
 evaluate(args.pred_path, args.ground_truth_path)
@@ -86,3 +109,7 @@ if args.get_missing_ingredients:
 
 if args.get_wrong_ingredients:
     get_wrong_ingredients(args.pred_path, args.ground_truth_path)
+
+if args.get_precision_recall_each_ingredient:
+    get_precision_recall_each_ingredient(
+        args.pred_path, args.ground_truth_path)
