6bff10ce5a3d476aef9c89c0f3bc8d7e5793d6b8
AnnyP <annipan@seas.upenn.edu>
2021-12-02 13:28:01 -0500

    Add script for getting the wrong predictions

--- evaluate.py
+++ evaluate.py
@@ -35,7 +35,21 @@ def get_missing_ingredients(pred_path, ground_truth_path):
                     ground_truth[ingredient]) - set(pred[ingredient]))
     with open(f'evaluations/missing_ingredients.json', 'w') as f_out:
         json.dump(missing_substitutes, f_out, ensure_ascii=False)
-    print("Missing ingredients can now be found at evaluations/missing_ingredients.json")
+    print("Missing substitutes can now be found at evaluations/missing_ingredients.json")
+
+
+def get_wrong_ingredients(pred_path, ground_truth_path):
+    with open(f'data/{ground_truth_path}') as f:
+        ground_truth = json.load(f)
+        with open(f'predictions/{pred_path}') as f_pred:
+            pred = json.load(f_pred)
+            wrong_substitutes = {}
+            for ingredient in ground_truth:
+                wrong_substitutes[ingredient] = list(set(
+                    pred[ingredient]) - set(ground_truth[ingredient]))
+    with open(f'evaluations/wrong_ingredients.json', 'w') as f_out:
+        json.dump(wrong_substitutes, f_out, ensure_ascii=False)
+    print("Wrong substitutes can now be found at evaluations/wrong_ingredients.json")
 
 
 def get_num_correct(true, pred):
@@ -61,7 +75,14 @@ parser.add_argument("-g", "--ground_truth", dest="ground_truth_path",
                     required=True, help="path to the ground truth file")
 parser.add_argument("-gm", "--get_missing", dest="get_missing_ingredients",
                     required=False, help="set to True to output the missing ingredients")
+parser.add_argument("-gw", "--get_wrong", dest="get_wrong_ingredients",
+                    required=False, help="set to True to output the wrong ingredients")
+
 args = parser.parse_args()
 evaluate(args.pred_path, args.ground_truth_path)
+
 if args.get_missing_ingredients:
     get_missing_ingredients(args.pred_path, args.ground_truth_path)
+
+if args.get_wrong_ingredients:
+    get_wrong_ingredients(args.pred_path, args.ground_truth_path)
