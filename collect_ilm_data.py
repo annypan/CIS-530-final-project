4b102cb40c1b96be9cb9f335c579e86faca09123
AnnyP <annipan@seas.upenn.edu>
2021-12-14 14:58:50 -0500

    Train-val-test split

--- collect_ilm_data.py
+++ collect_ilm_data.py
@@ -1,10 +1,32 @@
+import random
 import json
+import math
 
 if __name__ == '__main__':
     with open('data/recipes_with_nutritional_info.json') as f:
-        with open('data/instructions.txt', 'w') as f_out:
-            recipes = json.load(f)
-            for recipe in recipes:
-                for instruction in recipe['instructions']:
-                    f_out.write(instruction['text'] + '\n')
-            print('finished')
+        # with open('data/instructions.txt', 'w') as f_out:
+        #     recipes = json.load(f)
+        #     for recipe in recipes:
+        #         for instruction in recipe['instructions']:
+        #             f_out.write(instruction['text'] + '\n')
+        #     print('finished')
+        lines = []
+        recipes = json.load(f)
+        for recipe in recipes:
+            for instruction in recipe['instructions']:
+                lines.append(instruction['text'])
+        random.shuffle(lines)
+        size = len(lines)
+        train_lines = lines[:math.floor(size * 0.8)]
+        val_lines = lines[math.floor(size * 0.8):math.floor(size * 0.9)]
+        test_lines = lines[math.floor(size * 0.9):]
+        with open('data/train.txt', 'w') as f_out:
+            for line in train_lines:
+                f_out.write(line + '\n')
+        with open('data/valid.txt', 'w') as f_out:
+            for line in val_lines:
+                f_out.write(line + '\n')
+        with open('data/test.txt', 'w') as f_out:
+            for line in test_lines:
+                f_out.write(line + '\n')
+        print('finished')
