35e73b725e78e3f3f0e4f36d7b1f505f6abec347
AnnyP <annipan@seas.upenn.edu>
2021-12-15 18:12:12 -0500

    Update files

--- collect_ilm_data.py
+++ collect_ilm_data.py
@@ -10,23 +10,31 @@ if __name__ == '__main__':
         #         for instruction in recipe['instructions']:
         #             f_out.write(instruction['text'] + '\n')
         #     print('finished')
-        lines = []
-        recipes = json.load(f)
-        for recipe in recipes:
-            for instruction in recipe['instructions']:
+        recipes = []
+        recipes_json = json.load(f)
+        for recipe_json in recipes_json:
+            lines = []
+            for instruction in recipe_json['instructions']:
                 lines.append(instruction['text'])
-        random.shuffle(lines)
-        size = len(lines)
-        train_lines = lines[:math.floor(size * 0.8)]
-        val_lines = lines[math.floor(size * 0.8):math.floor(size * 0.9)]
-        test_lines = lines[math.floor(size * 0.9):]
+            recipes.append(lines)
+        random.shuffle(recipes)
+        size = len(recipes)
+        train_recipes = recipes[:math.floor(size * 0.8)]
+        val_recipes = recipes[math.floor(size * 0.8):math.floor(size * 0.9)]
+        test_recipes = recipes[math.floor(size * 0.9):]
         with open('data/train.txt', 'w') as f_out:
-            for line in train_lines:
-                f_out.write(line + '\n\n\n')
+            for lines in train_recipes:
+                for line in lines:
+                    f_out.write(line + '\n')
+                f_out.write('\n\n\n')
         with open('data/valid.txt', 'w') as f_out:
-            for line in val_lines:
-                f_out.write(line + '\n\n\n')
+            for lines in val_recipes:
+                for line in lines:
+                    f_out.write(line + '\n')
+                f_out.write('\n\n\n')
         with open('data/test.txt', 'w') as f_out:
-            for line in test_lines:
-                f_out.write(line + '\n\n\n')
+            for lines in test_recipes:
+                for line in lines:
+                    f_out.write(line + '\n')
+                f_out.write('\n\n\n')
         print('finished')
